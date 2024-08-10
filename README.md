22b2c497097c7ec3290897b50c19fd736ff14d31
Marcus <marcus-v@outlook.com>
2024-08-10 14:29:01 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# youtube_do_perigo
+
+A new Flutter project.
+
+You can use this in your phone to watch yt videos.
---
+++ android/app/src/main/kotlin/com/example/youtube_do_perigo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.youtube_do_perigo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
